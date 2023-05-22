e6bc8ac6dcde80797a89ad9fd3dd5e4d92b11f40
YusufBroujerdi <yusufbroujerdi@outlook.com>
2023-05-22 16:30:05 +0200

    Completes first version of code.

--- lang.py
+++ lang.py
@@ -1,12 +1,65 @@
-import json
+import pickle
 import ui
 from typing import Callable
+
+
+def adjust_collection(collection, io_searcher : Callable, adjuster : Callable, intention : str = 'deletion'):
+
+    obj = io_searcher(intention)
+
+    if (obj in collection) ^ (intention == 'deletion'):
+        adjuster(obj)
+        print(f'{intention} was successful.')
+    else:
+        print(f'{intention} was redundant. Nothing was changed.')
+
+
+def guess_words(collection : set, reverse_translation : bool = False):
+    
+    correctly_guessed_word_pairs = cg = set()
+    incorrectly_guessed_word_pairs = ig = set()
+    skipped_word_pairs = sg = set()
+
+    collection = {WordPair(word_pair.span, word_pair.eng) for word_pair in collection} if reverse_translation else collection
+    
+    for word_pair in collection:
+
+        word_guess = input(f'\nTranslate the word {word_pair.eng} (or enter 1 to skip or 0 to exit):')
+
+        match word_guess:
+
+            case word_pair.span:
+                print('\nCorrect!')
+                correctly_guessed_word_pairs.add(word_pair)
+            
+            case '1':
+                print('\nWord skipped.')
+                skipped_word_pairs.add(word_pair)
+            
+            case '0':
+                break
+
+            case _:
+                print('\nIncorrect.')
+                incorrectly_guessed_word_pairs.add(word_pair)
+    
+    print(f'Percentage guessed correctly: {(cg*100)/(cg+ig+sg)}')
+    print(f'Percentage guessed incorrectly: {(ig*100)/(cg+ig+sg)}')
+    print(f'Percentage skipped: {(sg*100)/(cg+ig+sg)}')
+    
+    collection = {WordPair(word_pair.span, word_pair.eng) for word_pair in collection} if reverse_translation else collection
+
+
 
 
 class WordPair:
 
     eng : str
     span : str
+
+    def __init__(self, eng, span):
+        self.eng = eng
+        self.span = span
 
     def __hash__(self):
         return hash( (self.eng, self.span) )
@@ -38,23 +91,33 @@ class LangLearner:
 
         print('Welcome to lang-learner.')
 
-        with open('data.txt') as saved_data:
-            self.word_sets = saved_data
+        self.word_sets = {'household furniture' : {WordPair('oven', 'el horno'), WordPair('toilet', 'el inodoro'), WordPair('shelf', 'el estante') \
+                                                   , WordPair('floor', 'el piso'), WordPair('rug', 'el tapete')} ,
+                          'academia' : {WordPair('word', 'la palabra'), WordPair('homework', 'la tarea'), WordPair('lesson', 'la clase')}
+        }
+
+        # with open('data.txt') as saved_data:
+        #     self.word_sets = saved_data
+        # saved_data.close()
 
         self.main_menu()
     
     def __del__(self):
 
-        with open('data.txt') as saved_data:
-            saved_data.write(json.dumps(self.word_sets))
+        pass
+
+        # with open('data.txt') as saved_data:
+        #     saved_data.write(json.dumps(self.word_sets))
+        # saved_data.close()
 
 
 
     def main_menu(self):
         
         print('\nMain menu:\n\n')
+        cats = self.word_sets
 
-        menu_items = [ui.Item(cat, f'navigate to category {cat}', self.navigate_category(cat)) for cat in self.word_sets.keys()]
+        menu_items = [ui.Item(cat, f'navigate to category {cat}', lambda: self.navigate_category(cat)) for cat in cats.keys()]
         menu_items += [ui.Item(1, 'add a new category', add_category),
                       ui.Item(2, 'remove a category', remove_category),
                       ui.Item(0, 'exit', lambda: print('\n Saving and exiting...'))]
@@ -63,33 +126,21 @@ class LangLearner:
         final_menu()
 
 
+        def io_searcher(intention : str):
+            return input(f'\nEnter the name of the category intended for {intention}')
+
         def add_category():
-
-            new_category = input(f'\nEnter the name of the new category you wish to create.')
-
-            if new_category not in self.word_sets.keys():
-                self.word_sets[new_category] = {}
-                print('\nNew category created.')
-            else:
-                print('\nNew category already exists! Nothing was changed.')
-
+            adjust_collection(cats.keys(), io_searcher, lambda cat : cats.update({ cat : {} }), intention = 'creation')
 
         def remove_category():
-            
-            old_category = input(f'\nEnter the name of the category you wish to remove.')
-
-            if old_category in self.word_sets.keys():
-                self.word_sets.pop(old_category)
-                print('\nCategory successfully removed.')
-            else:
-                print('\nCategory not found! Nothing was changed.')
+            adjust_collection(cats.keys(), io_searcher, cats.pop, intention = 'deletion')
 
     
     
     def navigate_category(self, category_name : str):
         
         print(f'\n category "{category_name}":\n\n')
-        word_pairs = self.word_sets[category_name]
+        pairs = self.word_sets[category_name]
 
         menu_items = [ui.Item(1, 'see word pairs', see_word_pairs),
                       ui.Item(2, 'add a new word pair', add_word_pair),
@@ -108,52 +159,53 @@ class LangLearner:
         def see_word_pairs():
             
             print(f'\nAll word pairs:\n')
-            [print(word_pair) for word_pair in word_pairs]
+            [print(word_pair) for word_pair in pairs]
             self.navigate_category(category_name)
         
 
+        def io_searcher(intention : str):
+
+            eng = input(f'\nEnter the spanish word of the pair intended for {intention}')
+            span = input(f'\nEnter the english word of the pair intended for {intention}')
+            return WordPair(eng, span)
+
+
         def add_word_pair():
-            
-            new_pair = WordPair()
-            new_pair.span = input(f'\nEnter the spanish word of the new word pair:')
-            new_pair.eng = input(f'\nEnter the english word of the new word pair:')
-
-            if new_pair not in word_pairs:
-                word_pairs.add(new_pair)
-                print(f'\nNew pair successfully added.')
-            else:
-                print(f'\nPair already exists. Nothing was changed.')
-
+            adjust_collection(pairs, io_searcher, pairs.add, intention = 'creation')
 
         def remove_word_pair():
-            
-            pair = WordPair()
-            pair.span = input(f'\nEnter the spanish word of the pair to delete:')
-            pair.eng = input(f'\nEnter the english word of the pair to delete:')
-
-            if pair in word_pairs:
-                word_pairs.delete(pair)
-                print(f'\nPair successfully deleted.')
-            else:
-                print(f'\nPair does not exist. Nothing was changed.')
+            adjust_collection(pairs, io_searcher, pairs.delete, intention = 'deletion')
 
 
         def see_sub_categories():
             
-            pass
+            menu_items = [ui.Item(cat, f'navigate to {cat}', lambda: self.navigate_category(cat)) \
+                          for cat in self.word_sets.keys() if self.word_sets[cat] < pairs]
+            menu_items.append(ui.Item(0, f'return to original category', lambda: self.navigate_category(category_name)))
+
+            sub_category_menu : Callable = ui.generate_menu(menu_items)
+            sub_category_menu()
+            
 
         def copy_category():
-            pass
+            
+            menu_items = [ui.Item(cat, f'copy {category_name} into {cat}', lambda: cat.update(pairs)) \
+                           for cat in self.word_sets.keys()]
+            menu_items.append(ui.Item(0, f'abort copying', lambda: self.navigate_category(category_name)))
+
+            copy_category_menu : Callable = ui.generate_menu(menu_items)
+            copy_category_menu()           
+
 
         def guess_english():
-            pass
+            guess_words(pairs, True)
 
         def guess_spanish():
-            pass
+            guess_words(pairs)
 
 
 
 
 if __name__ == '__main__':
 
-    WordPair()
+    LangLearner()
